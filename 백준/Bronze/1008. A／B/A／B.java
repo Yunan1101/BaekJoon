e05942323a73477b5a3aee487e7309c6e5b6f868
Yunan1101 <143979445+Yunan1101@users.noreply.github.com>
2024-04-03 23:24:10 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        int a;
+        int b;
+        double result;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        result = (double)a/b;
+        System.out.println(result);
+    }
+}

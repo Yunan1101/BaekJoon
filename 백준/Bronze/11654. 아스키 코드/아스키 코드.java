7e46dbe9d600968720d185ad02ca476bec6fc631
Yunan1101 <143979445+Yunan1101@users.noreply.github.com>
2025-07-24 19:30:30 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String T = scanner.nextLine();
+        int n = (int)T.charAt(0);
+
+        System.out.println(n);
+    }
+}
